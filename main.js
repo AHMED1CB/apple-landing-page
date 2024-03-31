abe5ae1a7eff96a70895619f9ab4d4e609280f5b
AHMED HASSAN <151830489+AHMED1CB@users.noreply.github.com>
2024-03-31 09:32:54 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let swaping = document.querySelector('.swaper')
+let title = document.querySelector('h2')
+let para = document.querySelector('p')
+function swap(src , color) {
+swaping.src = src;
+document.body.style.background = color;
+}
